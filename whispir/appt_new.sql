b990f894468d5ec8e50f16a6fd3f8ba0340e71c7
jeebendu <jeebendu2010@gmail.com>
2025-04-29 03:14:54 +0530

    Update appt_new.sql

--- whispir/appt_new.sql
+++ whispir/appt_new.sql
@@ -20,6 +20,57 @@ BEGIN
 
     DECLARE @offset INT = (@pageNumber - 1) * @pageSize;
 
+    -- Determine the column to filter based on @dateType
+    DECLARE @dateColumn NVARCHAR(50);
+    IF @dateType = '0'
+        SET @dateColumn = 'appt.date_of_submission';
+    ELSE IF @dateType = '1'
+        SET @dateColumn = 'appt.requestoptionldate';
+    ELSE IF @dateType = '2'
+        SET @dateColumn = 'appt.confirmedappointmentdate';
+
+    -- Common Table Expression (CTE) for filtered data
+    WITH FilteredData AS (
+        SELECT ARSR.*, appt.*, apptStatus.*
+        FROM Apptrequeststatusrecord ARSR
+        JOIN appointment_request appt ON ARSR.appointment_request_id = appt.id
+        JOIN appointment_requeststatus apptStatus ON ARSR.appointment_requeststatus_id = apptStatus.id
+        WHERE 1 = 1
+            -- Apply filters
+            AND (
+                @status IS NULL OR @status = ''
+                OR (
+                    @status = 'myown' AND (
+                        (LOWER(apptStatus.name) = 'assigned' OR LOWER(apptStatus.name) = 'pending')
+                        AND appt.casemanagerid = @caseManagerId
+                    )
+                )
+                OR (
+                    LOWER(@status) = 'pending' AND LOWER(apptStatus.name) = 'pending'
+                )
+                OR (
+                    LOWER(@status) = 'new' AND (
+                        LOWER(apptStatus.name) = 'assigned' OR LOWER(apptStatus.name) = 'new'
+                    )
+                )
+                OR (
+                    LOWER(apptStatus.name) = LOWER(@status)
+                )
+            )
+            AND (@caseManagerId IS NULL OR @caseManagerId = 0 OR appt.casemanagerid = @caseManagerId)
+            AND (
+                @type IS NULL OR @type = '' OR @value IS NULL OR @value = ''
+                OR LOWER(appt.@type) LIKE '%' + LOWER(@value) + '%'
+            )
+            AND (
+                @dateColumn IS NULL
+                OR (
+                    (@fromDate IS NULL OR CONVERT(DATETIME, @dateColumn) >= @fromDate)
+                    AND (@toDate IS NULL OR CONVERT(DATETIME, @dateColumn) <= @toDate)
+                )
+            )
+    )
+
     -- Total Count (without filters)
     DECLARE @totalCount INT;
     SELECT @totalCount = COUNT(*)
@@ -28,111 +79,11 @@ BEGIN
     -- Filtered Count (with filters)
     DECLARE @filteredCount INT;
     SELECT @filteredCount = COUNT(*)
-    FROM Apptrequeststatusrecord ARSR
-    JOIN appointment_request appt ON ARSR.appointment_request_id = appt.id
-    JOIN appointment_requeststatus apptStatus ON ARSR.appointment_requeststatus_id = apptStatus.id
-    WHERE 1 = 1
-        -- Apply filters (same as below for paginated data)
-        AND (
-            @status IS NULL OR @status = ''
-            OR (
-                @status = 'myown' AND (
-                    (LOWER(apptStatus.name) = 'assigned' OR LOWER(apptStatus.name) = 'pending')
-                    AND appt.casemanagerid = @caseManagerId
-                )
-            )
-            OR (
-                LOWER(@status) = 'pending' AND LOWER(apptStatus.name) = 'pending'
-            )
-            OR (
-                LOWER(@status) = 'new' AND (
-                    LOWER(apptStatus.name) = 'assigned' OR LOWER(apptStatus.name) = 'new'
-                )
-            )
-            OR (
-                LOWER(apptStatus.name) = LOWER(@status)
-            )
-        )
-        AND (@caseManagerId IS NULL OR @caseManagerId = 0 OR appt.casemanagerid = @caseManagerId)
-        AND (
-            @type IS NULL OR @type = '' OR @value IS NULL OR @value = ''
-            OR LOWER(appt.@type) LIKE '%' + LOWER(@value) + '%'
-        )
-        AND (
-            @dateType IS NULL OR @dateType = ''
-            OR (
-                @dateType = '0' AND (
-                    (@fromDate IS NULL OR appt.date_of_submission >= @fromDate)
-                    AND (@toDate IS NULL OR appt.date_of_submission <= @toDate)
-                )
-            )
-            OR (
-                @dateType = '1' AND (
-                    (@fromDate IS NULL OR appt.requestoptionldate >= @fromDate)
-                    AND (@toDate IS NULL OR appt.requestoptionldate <= @toDate)
-                )
-            )
-            OR (
-                @dateType = '2' AND (
-                    (@fromDate IS NULL OR appt.confirmedappointmentdate >= @fromDate)
-                    AND (@toDate IS NULL OR appt.confirmedappointmentdate <= @toDate)
-                )
-            )
-        );
+    FROM FilteredData;
 
     -- Paginated Data
     SELECT @totalCount AS totalCount, @filteredCount AS filteredCount, *
-    FROM Apptrequeststatusrecord ARSR
-    JOIN appointment_request appt ON ARSR.appointment_request_id = appt.id
-    JOIN appointment_requeststatus apptStatus ON ARSR.appointment_requeststatus_id = apptStatus.id
-    WHERE 1 = 1
-        -- Apply filters
-        AND (
-            @status IS NULL OR @status = ''
-            OR (
-                @status = 'myown' AND (
-                    (LOWER(apptStatus.name) = 'assigned' OR LOWER(apptStatus.name) = 'pending')
-                    AND appt.casemanagerid = @caseManagerId
-                )
-            )
-            OR (
-                LOWER(@status) = 'pending' AND LOWER(apptStatus.name) = 'pending'
-            )
-            OR (
-                LOWER(@status) = 'new' AND (
-                    LOWER(apptStatus.name) = 'assigned' OR LOWER(apptStatus.name) = 'new'
-                )
-            )
-            OR (
-                LOWER(apptStatus.name) = LOWER(@status)
-            )
-        )
-        AND (@caseManagerId IS NULL OR @caseManagerId = 0 OR appt.casemanagerid = @caseManagerId)
-        AND (
-            @type IS NULL OR @type = '' OR @value IS NULL OR @value = ''
-            OR LOWER(appt.@type) LIKE '%' + LOWER(@value) + '%'
-        )
-        AND (
-            @dateType IS NULL OR @dateType = ''
-            OR (
-                @dateType = '0' AND (
-                    (@fromDate IS NULL OR appt.date_of_submission >= @fromDate)
-                    AND (@toDate IS NULL OR appt.date_of_submission <= @toDate)
-                )
-            )
-            OR (
-                @dateType = '1' AND (
-                    (@fromDate IS NULL OR appt.requestoptionldate >= @fromDate)
-                    AND (@toDate IS NULL OR appt.requestoptionldate <= @toDate)
-                )
-            )
-            OR (
-                @dateType = '2' AND (
-                    (@fromDate IS NULL OR appt.confirmedappointmentdate >= @fromDate)
-                    AND (@toDate IS NULL OR appt.confirmedappointmentdate <= @toDate)
-                )
-            )
-        )
+    FROM FilteredData
     ORDER BY appt.id
     OFFSET @offset ROWS FETCH NEXT @pageSize ROWS ONLY;
 END;
